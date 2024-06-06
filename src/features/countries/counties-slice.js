9a1643ba18affec357def84a35d71d8387f5e4c6
Ekaterina Trifonova <ekaterinatrifonova@Ekaterinas-MacBook-Pro.local>
2024-06-06 10:50:57 +0300

    feat: Add filtering countries by region

---
+++ src/features/countries/counties-slice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadCountries = createAsyncThunk(
+  'countries/loadCountries',
+  async (_,  {
+    extra: { client, api }
+  }) => {
+    return client.get(api.ALL_COUNTRIES)
+  }
+);
+
+const initialState = {
+  status: 'idle',
+  error: null,
+  list: [],
+}
+const countrySlice = createSlice({
+  name: '@@countries',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadCountries.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(loadCountries.fulfilled, (state, action) => {
+        state.status = 'received';
+        state.list = action.payload.data;
+      })
+      .addCase(loadCountries.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.payload || action.meta.error;
+      })
+  }
+})
+
+export const countryReducer = countrySlice.reducer;
+
+// Selectors
+export const selectCountriesInfo = (state) => ({
+  status: state.countries.status,
+  error: state.countries.error,
+  qty: state.countries.list.length,
+})
+
+export const selectAllCountries = (state) => state.countries.list;
+export const selectVisibleCountries = (state, {search = '', region = ''}) => state.countries.list
+  .filter(c => c.name.common.toLowerCase().includes(search.toLowerCase()) && c.region.includes(region));
--- src/store.js
+++ src/store.js
@@ -6,11 +6,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { themeReducer } from './features/theme/theme-slice';
 import { controlsReducer } from './features/controls/controls-slice';
+import { countryReducer } from './features/countries/counties-slice';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     controls: controlsReducer,
+    countries: countryReducer,
   },
   devtools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
